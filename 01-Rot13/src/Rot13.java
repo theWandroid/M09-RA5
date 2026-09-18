27cbae67e49c5f761c326a0a09ff8d55acfcc9c6
theWandroid <azarin.andrei@gmail.com>
2026-09-18 13:31:32 +0200

    Activitat Rot13 inicialitzada

---
+++ 01-Rot13/src/Rot13.java
@@ -0,0 +1,13 @@
+
+
+public class Rot13{
+    
+        private char[] minuscles = {'a', 'á', 'à', 'b', 'c', 'ç', 'd', 'e', 'é', 'è', 'f', 'g', 'h', 'i', 'í', 'ì', 'ï', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'ó', 'ò', 'p', 'q', 'r', 's', 't', 'u', 'ú', 'ù', 'ü', 'v', 'w', 'x', 'y', 'z'};
+        private char[] majuscles = {'A', 'Á', 'À', 'B', 'C', 'Ç', 'D', 'E', 'É', 'È', 'F', 'G', 'H', 'I', 'Í', 'Ì', 'Ï', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'Ó', 'Ò', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ú', 'Ù', 'Ü', 'V', 'W', 'X', 'Y', 'Z'};
+        
+    static void main(String[] args){
+        System.out.println("Hello World!");
+    
+}
+
+}
